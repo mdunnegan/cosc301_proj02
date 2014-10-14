35ad1b5d06a6dba15b0b1ebb30eec2b2aaa3dc86
zlazow <zlazow@colgate.edu>
2014-10-14 14:32:01 -0400

    Tokenify/Parsing Attempt

--- main.c
+++ main.c
@@ -39,7 +39,7 @@ char** tokenify(const char *s) {
 		if (strstr(token, "#")==NULL){
 		//if the line contains a #, ignore the rest of the tokens.
 			words[count]=strdup(token);
-			token = strtok(NULL, " \n\t");
+			token = strtok(NULL, ";");
 			count++;	
 		}
 		else{
@@ -55,27 +55,85 @@ char** tokenify(const char *s) {
 	}
 	words[count]=NULL;
 	free(copy);
+	for (int i=0; i<count; i++){
+		printf("Token %i: %s\n", i, words[i]);
+		fflush(stdout);
+	}
 	return words;
 	
 	
 }
+// parses a single command, returns a pointer to each element
+// first element will be the file path
+// all following elements will be flags
+char** parse_command(const char *s) {
+    char *str = strdup(s);
+    int index = 0;
 
+    int numWhiteSpaces = 0;
+    for (int i = 0; i < strlen(s); i++){
+    	if (isspace(s[i])){
+    	    numWhiteSpaces++;
+    	}
+    }
 
+    char **returnArray = (char**) malloc(sizeof(char*) * numWhiteSpaces+1);
+    char *token = malloc(sizeof(char));
+    token = strtok(str, " \n\t");
+    return token;
+
+    while (token != NULL){
+    	token = strdup(token);
+    	returnArray[index] = token;
+    	index++;
+    	token = strtok(NULL, " \n\t");
+    }
+    returnArray[index] = NULL;
+    return returnArray; // pointer to rv
+}
+
+int validate_command(const char **s){
+	// make a copy
+
+	char** copy = strdup(s);
+	printf("The copy: %s\n", copy);
+	printf("Copy[0]: %c\n", copy[0]);
+
+	if (copy[0] != "/"){
+		return 0; // false, file path must start with slash
+	}
+
+}
+void print_tokens(char *tokens[]) {
+    int i = 0;
+    while (tokens[i] != NULL) {
+        printf("Token %d: %s\n", i+1, tokens[i]);
+        i++;
+    }
+}
 
 int main(int argc, char **argv) {
+	//testing
+	char *test = "/bin/ls -p -f";
+	char *command = parse_command(test);
+	print_tokens(&command);
+	validate_command(&command);
+
 	//get Input
-	int sequential = 1
+	int sequential = 1;
 	int count = 0;
 	printf("Prompt: ");
 	fflush(stdout);  
 	char line[1024];
 	while (fgets(line, 1024, stdin) != NULL) {
-		if (count!=0){
-			printf("Prompt: ");
+		for (int i =0; i<1024; i++){
+			if (line[i]=='\n'){
+				line[i]='\0';
+			}
 		}
 		if (strcmp(line, "exit")==0){
 			printf("Exiting \n");
-			wait(); //wait for all other processes to finish.
+			//wait(); //wait for all other processes to finish.
 			return 0;
 		}
 		if (strcmp(line, "mode")==0){
@@ -101,12 +159,13 @@ int main(int argc, char **argv) {
 		if (sequential) {
 			// sequential stuff
 		}
-		else () {
+		else {
 			// parallel stuff
 		}
+		printf("Prompt: ");
+		
 
-
-	count ++;
+		count ++;
 	}
 
 
